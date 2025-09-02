9cb95cc43118e20b1819177fa4dd4140d21e227b
Thibault Mir <thibault.mir@servier.com>
2025-09-02 16:23:04 +0200

    fix: force FolioReader full screen presentation

--- Source/FolioReaderKit.swift
+++ Source/FolioReaderKit.swift
@@ -151,18 +151,34 @@ open class FolioReader: NSObject {
 
 extension FolioReader {
 
-    /// Present a Folio Reader Container modally on a Parent View Controller.
-    ///
-    /// - Parameters:
-    ///   - parentViewController: View Controller that will present the reader container.
-    ///   - epubPath: String representing the path on the disk of the ePub file. Must not be nil nor empty string.
-	///   - unzipPath: Path to unzip the compressed epub.
-    ///   - config: FolioReader configuration.
-    ///   - shouldRemoveEpub: Boolean to remove the epub or not. Default true.
-    ///   - animated: Pass true to animate the presentation; otherwise, pass false.
-    open func presentReader(parentViewController: UIViewController, withEpubPath epubPath: String, unzipPath: String? = nil, andConfig config: FolioReaderConfig, shouldRemoveEpub: Bool = true, animated:
-        Bool = true) {
-        let readerContainer = FolioReaderContainer(withConfig: config, folioReader: self, epubPath: epubPath, unzipPath: unzipPath, removeEpub: shouldRemoveEpub)
+    open func presentReader(
+        parentViewController: UIViewController,
+        withEpubPath epubPath: String,
+        unzipPath: String? = nil,
+        andConfig config: FolioReaderConfig,
+        shouldRemoveEpub: Bool = true,
+        animated: Bool = true
+    ) {
+        let readerContainer = FolioReaderContainer(
+            withConfig: config,
+            folioReader: self,
+            epubPath: epubPath,
+            unzipPath: unzipPath,
+            removeEpub: shouldRemoveEpub
+        )
+
+        // 👉 Forcer le plein écran (iPad/iPhone)
+        readerContainer.modalPresentationStyle = .fullScreen
+        readerContainer.isModalInPresentation = true
+
+        // (Optionnel: si jamais iPad force quand même une sheet, on étire au max)
+        if #available(iOS 15.0, *), let sheet = readerContainer.sheetPresentationController {
+            sheet.detents = [.large()]
+            sheet.prefersGrabberVisible = false
+            sheet.largestUndimmedDetentIdentifier = .large
+            sheet.selectedDetentIdentifier = .large
+        }
+
         self.readerContainer = readerContainer
         parentViewController.present(readerContainer, animated: animated, completion: nil)
         addObservers()
